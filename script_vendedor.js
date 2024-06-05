ed5a88395f7246a9b8d8c294b00e369f338ea8a8
Marcus Vinicius <102832530+Blue02z@users.noreply.github.com>
2024-06-04 21:04:39 -0300

    Add files via upload

---
+++ script_vendedor.js
@@ -0,0 +1,15 @@
+let like = document.querySelectorAll(".like");
+let curtidas = document.querySelectorAll(".curtidas");
+
+
+curtidas.forEach((element, oi) => {
+    like[oi].addEventListener("click", () =>{
+
+        like[oi].classList.toggle("ativado")
+
+
+        
+
+        
+    }) 
+});
