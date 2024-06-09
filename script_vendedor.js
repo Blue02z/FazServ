6d60f1d3384db12b0f857245d2db92a33703924d
Marcus Vinicius <102832530+Blue02z@users.noreply.github.com>
2024-06-09 12:46:10 -0300

    Add files via upload

--- script_vendedor.js
+++ script_vendedor.js
@@ -2,6 +2,7 @@ let like = document.querySelectorAll(".like");
 let curtidas = document.querySelectorAll(".curtidas");
 let btnMenu = document.querySelector(".menu-icon")
 let menuAberto = document.querySelector(".menu-aberto")
+let body = document.body
 
 curtidas.forEach((element, oi) => {
     like[oi].addEventListener("click", () =>{
